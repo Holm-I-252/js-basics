4ba324b085cece63b08bb8c6aa82aa27611d23e1
Ian Holm <ianholm252@gmail.com>
2021-07-30 12:03:01 -0600

    Completed project with enhanced password checker

--- password.js
+++ password.js
@@ -6,19 +6,24 @@ const rl = readline.createInterface({
 });
 
 
-rl.question("Welcome to the password validator! Please enter a password at least 10 characters long and has one special character or number: ", function (answer) {
-    let allLetters = false
+rl.question("Welcome to the password validator! Please enter a password at least 10 characters long and has a combination of at least one letter and digit: ", function (answer) {
+    let allLetters = false;
+    let allNumbers = false;
     if (/^[a-zA-Z]+$/.test(answer)) {
         allLetters = true;
     }
 
-    if (answer.length >= 10 && allLetters === false) {
-        console.log(`Success! ${answer} is validated!`);
+    if (/^\d+$/.test(answer)) {
+        allNumbers = true;
+    }
+
+    if (answer.length >= 10 && allLetters === false && allNumbers === false) {
+        console.log(`Success! ${answer} is accepted!`);
     }
     else {
         console.log(`${answer} doesn't work. Please try again.`);
     }
-    
+
     rl.close();
 })
 
